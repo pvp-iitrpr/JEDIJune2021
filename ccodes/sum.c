a9c06df50246a053f131ec02ed3cd2eca0643deb
pvp-iitrpr <2017eeb1157@iitrpr.ac.in>
2021-06-17 10:21:07 +0530

    Changes committed: 	modified:   ccodes/sum.c 	modified:   javacodes/hello.java

--- ccodes/sum.c
+++ ccodes/sum.c
@@ -1,5 +1,6 @@
  this is the sum
 void sum(int x,int y){
 printf("THe sum is %d",x+y);
+printf("The sum again is %d",x+y);
 #here again
 }
--- javacodes/hello.java
+++ javacodes/hello.java
@@ -2,4 +2,8 @@ class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
     }
+    public void hey(){
+    	System.out.println("I am in hey method");
+    
+    }
 }
